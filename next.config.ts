713752adcc285b6f8c435239077ad8c6583d3422
Abhishek91K <abhishek@codewithabhishek.in>
2026-04-02 16:08:25 +0530

    fix(build): add experimental.cpus: 1 to fix Vercel Hobby build hang

--- next.config.ts
+++ next.config.ts
@@ -31,47 +31,32 @@ const securityHeaders = [
 ];
 
 const nextConfig: NextConfig = {
+  // ← Minimal but important addition
+  experimental: {
+    cpus: 1, // Limits build to 1 CPU core (prevents OOM/hang on Hobby)
+  },
+
   async headers() {
     return [{ source: "/:path*", headers: securityHeaders }];
   },
+
   images: {
     remotePatterns: [{ hostname: "res.cloudinary.com" }],
   },
 };
 
 export default withSentryConfig(withNextIntl(nextConfig), {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "abhishek-kumar-oe",
-
   project: "kridha",
 
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
+  // Keep your existing Sentry options
   webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
     treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
   },
